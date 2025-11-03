70f9ea718c5bee7e908c6d7767967a1a70a9e6d6
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2025-11-03 11:29:35 +0100

    Changed method name in TicTacToe + added starting code to Main

--- src/se/yrgo/TicTacToe/Main.java
+++ src/se/yrgo/TicTacToe/Main.java
@@ -1,7 +1,37 @@
 package se.yrgo.TicTacToe;
 
+import javax.xml.transform.Source;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Player player1 = new Player(String playerMarker"X");
+        Player player2 = new Player(String playerMarker"O");
+        Grid gameBoard = new Grid;
+        Scanner scanner = new Scanner(System.in)
+        String player1Choice;
+        String player2Choice;
 
-    }
+        System.out.println("Hej och välkommen till tre i rad!");
+        System.out.println("Skriv in spelare 1:s namn");
+        player1.setName(scanner.nextLine());
+        System.out.println("Skriv in spelare 2:s namn");
+        player2.setName(scanner.nextLine());
+
+
+        System.out.println("Regler: välj en tom ruta genom att skriva en bokstav A-I. Första spelaren som för 3 i rad vinner. För att avsluta, skriv Stopp.");
+
+        gameBoard.printLetters();
+        System.out.printf("%n%n");
+
+        while (true)
+            System.out.println("Spelare 1: Välj en tom ruta.");
+        player1Choice = scanner.nextLine();
+            if (player1Choice.toLowerCase() = Stopp){
+                break;
+            }
+
+
+}
 }
--- src/se/yrgo/TicTacToe/TicTacToe.java
+++ src/se/yrgo/TicTacToe/TicTacToe.java
@@ -4,7 +4,7 @@ import java.sql.SQLOutput;
 
 public class TicTacToe {
 
-    public static void playerMove(Player player, Grid[][] field){
+    public static void addMarker(Player player, Grid[][] field){
 
    if (rutan är enum empty){
        set player marker;
