777d3a083d7820546264f1a4293fd0b5cff2da08
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2025-11-03 09:57:07 +0100

    Add main method in main + welcome msg

--- src/se/yrgo/TicTacToe/TicTacToe.java
+++ src/se/yrgo/TicTacToe/TicTacToe.java
@@ -1,4 +1,12 @@
 package se.yrgo.TicTacToe;
 
+import java.sql.SQLOutput;
+
 public class TicTacToe {
+    public static void main(String[] args){
+
+
+    System.out.println("Välkommen till Tre i rad!");
+
+    }
 }
