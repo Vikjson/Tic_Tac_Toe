a7da79e03bb252cd68465204340beb6acde3c6c8
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2025-11-03 11:01:49 +0100

    Sketching changes in TicTacToe and Grid

--- src/se/yrgo/TicTacToe/Grid.java
+++ src/se/yrgo/TicTacToe/Grid.java
@@ -1,5 +1,21 @@
 package se.yrgo.TicTacToe;
 
 public class Grid {
+    private GridItems[][] gameBoard;
+
+    public Grid() {
+       topLeft = emptyBox;
+       colDiv1 = colD
+
+        gameBoard = {
+                {topLeft, colDiv1, topMid, ColDiv2, topRight},
+                {underLine1, plus1, underLine2, plus2, underLine3},
+                {midLeft, colDiv3, midMid, ColDiv4, midRight},
+                {underLine4, plus3, underLine5, plus4, underLine6},
+                {bottomLeft, Div5, bottomMid, ColDiv6, bottomRight}
+        };
+
+
+    }
 
 }
